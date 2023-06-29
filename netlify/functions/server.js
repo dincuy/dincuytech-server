5942e3dd2022b9655a0fc969074325c612383d03
Dincuy23 <dincuy23@gmail.com>
2023-06-29 17:13:48 +0000

    netlify deploy

---
+++ netlify/functions/server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const serverless = require("serverless-http");
+const cors = require("cors");
+const corsOptions = require("../../config/corsOptions");
+
+// const PORT = process.env.PORT || 3500
+
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.send("Hallo home");
+});
+
+app.use("/checkout", require("../../routes/checkout"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  res.json({ message: "Url not found cuyy" });
+});
+
+// app.listen(PORT, () => {
+//   console.log(`Sukses server berjalan di http://localhost:${PORT}`)
+// })
+
+const handler = serverless(app);
+
+module.exports = { handler };
