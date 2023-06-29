b2fd6b7afadfe2cb85c186e184f11e2c875f816d
Dincuy23 <dincuy23@gmail.com>
2023-06-29 18:19:10 +0000

    deploy 3

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-  "https://dincuytech.netlify.app"
+  "https://dincuytech.netlify.app",
+  "https://3000-dincuy-dincuytech-jq22l6rl0p5.ws-us101.gitpod.io"
 ]
 
 module.exports = allowedOrigins
