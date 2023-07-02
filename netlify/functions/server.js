c359f06fd6e3dda97bf1ce4db306334a63d48587
Dincuy23 <dincuy23@gmail.com>
2023-07-02 11:34:44 +0000

    huh

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,43 +1,41 @@
-// require("dotenv").config();
-// const express = require("express");
-// const path = require('path')
-// const app = express();
+require("dotenv").config();
+const express = require("express");
+const path = require('path')
+const app = express();
 const serverless = require("serverless-http");
-// const cors = require("cors");
-// const corsOptions = require("../../config/corsOptions");
-// const { updateDataProduct } = require("../../controllers/productController");
+const cors = require("cors");
+const corsOptions = require("../../config/corsOptions");
+const { updateDataProduct } = require("../../controllers/productController");
 
 
-// // const PORT = process.env.PORT || 3500
+// const PORT = process.env.PORT || 3500
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
-// app.set("view engine", "ejs")
-// app.set('views', path.join(__dirname, '..', '..'));
+app.set("view engine", "ejs")
+app.set('views', path.join(__dirname, '..', '..'));
 
-// // timer dalam milidetik
-// // 24 jam
-// const timer = updateDataProduct(3600000)
+// timer dalam milidetik
+// 24 jam
+const timer = updateDataProduct(3600000)
 
-// app.get("/", (req, res) => {
-//   // const waktu = updateDataProduct()
+app.get("/", (req, res) => {
+  // const waktu = updateDataProduct()
   
-//   res.render('index', {timer: timer})
-// });
+  res.render('index', {timer: timer})
+});
 
-// app.use("/checkout", require("../../routes/checkout"));
+app.use("/checkout", require("../../routes/checkout"));
 
-// app.all("*", (req, res) => {
-//   res.status(404);
-//   res.json({ message: "Url not found cuyy" });
-// });
+app.all("*", (req, res) => {
+  res.status(404);
+  res.json({ message: "Url not found cuyy" });
+});
 
 // app.listen(PORT, () => {
 //   console.log(`Sukses server berjalan di http://localhost:${PORT}`)
 // })
-const { newApp } = require("../../server");
 
-
-const handler = serverless(newApp);
+const handler = serverless(app);
 
 module.exports = { handler };
--- server.js
+++ server.js
@@ -5,21 +5,21 @@ const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
 const { updateDataProduct } = require("./controllers/productController");
 
-// const PORT = 3500;
+const PORT = 3500
 
 app.use(cors(corsOptions));
 // app.use(express.static(path.join(__dirname, 'public')))
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs")
 
 // timer dalam milidetik
 // 24 jam
-const timer = updateDataProduct(3600000);
+const timer = updateDataProduct(3600000)
 
 app.get("/", (req, res) => {
   // const waktu = updateDataProduct()
-
-  res.render("index", { timer: timer });
+  
+  res.render('index', {timer: timer})
 });
 
 app.use("/checkout", require("./routes/checkout"));
@@ -29,8 +29,6 @@ app.all("*", (req, res) => {
   res.json({ message: "Url not found cuyy" });
 });
 
-// app.listen(PORT, () => {
-//   console.log(`Sukses server berjalan di http://localhost:${PORT}`);
-// });
-
-module.exports = { newApp: app };
+app.listen(PORT, () => {
+  console.log(`Sukses server berjalan di http://localhost:${PORT}`)
+})
