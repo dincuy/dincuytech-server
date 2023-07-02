18432acb388ddb89196a6b09e28a244069616d89
Dincuy23 <dincuy23@gmail.com>
2023-07-02 11:32:38 +0000

    fix semoga

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -2,7 +2,7 @@
 // const express = require("express");
 // const path = require('path')
 // const app = express();
-// const serverless = require("serverless-http");
+const serverless = require("serverless-http");
 // const cors = require("cors");
 // const corsOptions = require("../../config/corsOptions");
 // const { updateDataProduct } = require("../../controllers/productController");
