51c3a7f05918a28c4c730e57edc4f852d3ad5312
Dincuy23 <dincuy23@gmail.com>
2023-08-13 16:16:19 +0000

    tambah environment

--- config/firebase.js
+++ config/firebase.js
@@ -1,7 +1,24 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
+const dotenv = require('dotenv');
 
-const serviceAccount = require('./serviceAccountKey.json');
+dotenv.config(); // Mengatur environment variables dari file .env
+
+// const serviceAccount = require('./serviceAccountKey.json');
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: "dincuytech",
+  private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+  client_email: "firebase-adminsdk-jahox@dincuytech.iam.gserviceaccount.com",
+  client_id: "115886293658291098967",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-jahox%40dincuytech.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com"
+}
 
 initializeApp({
   credential: cert(serviceAccount)
