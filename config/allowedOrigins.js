0c8f99c11c921e18747a4ca3973fc4711d53cfc9
Dincuy <66787508+dincuy@users.noreply.github.com>
2024-05-23 07:08:44 +0000

    allow origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   "https://dincuytech.netlify.app",
   "https://3000-dincuy-dincuytech-7avxc3nnuql.ws-us101.gitpod.io",
   "http://127.0.0.1:3000",
-  "https://vitejsviteee1xgn-klhh--5173--41fbae16.local-credentialless.webcontainer.io",
+  "https://vitejsviteee1xgn-klhh--5173--bbe9fa9f.local-credentialless.webcontainer.io",
 ]
 
 module.exports = allowedOrigins
