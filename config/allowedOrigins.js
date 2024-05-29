f2ab46d9b3de11f3615166a9fbfa8c245544b413
Dincuy <66787508+dincuy@users.noreply.github.com>
2024-05-29 13:12:52 +0000

    for all ip

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "https://3000-dincuy-dincuytech-7avxc3nnuql.ws-us101.gitpod.io",
   "http://127.0.0.1:3000",
   "https://vitejsviteee1xgn-klhh--5173--bbe9fa9f.local-credentialless.webcontainer.io",
+  "http://0.0.0.0.0"
 ]
 
 module.exports = allowedOrigins
